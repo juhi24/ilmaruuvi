8bfcff0f615c58cc1f31f00cd3bbf26f2fb11e6a
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-04 14:51:00 +0200

    while true

--- logger.py
+++ logger.py
@@ -3,9 +3,12 @@
 """
 @author: Jussi Tiira
 """
+import time
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
 MAC1 = 'E8:A8:F2:B1:AE:A3'
 sensor = RuuviTagSensor(MAC1)
-state = sensor.update()
-print(state)
+while True:
+    state = sensor.update()
+    print(state)
+    time.sleep(10)
