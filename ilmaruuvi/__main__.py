553d76006a20dbc18ff55a9b79fe708d5a96f994
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-14 19:42:26 +0200

    newline

--- ilmaruuvi/__main__.py
+++ ilmaruuvi/__main__.py
@@ -3,13 +3,14 @@ import time
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
 def main():
+    database_file = '/var/lib/ilmaruuvi/db.hdf'
     logfile = '/var/log/ilmaruuvi.log'
     MAC1 = 'E8:A8:F2:B1:AE:A3'
     sensor = RuuviTagSensor(MAC1)
     while True:
         state = sensor.update()
         with open(logfile, 'a') as f:
-            f.write(str(state))
+            f.write(str(state + '\n'))
         time.sleep(10)
 
 if __name__ == "__main__":
