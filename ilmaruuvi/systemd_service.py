b168527eee0d732f3189d6e28e8843dcc5778c76
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-14 18:52:05 +0200

    fix

--- ilmaruuvi/systemd_service.py
+++ ilmaruuvi/systemd_service.py
@@ -6,7 +6,7 @@ def install():
     filename = 'ilmaruuvi.service'
     install_path = path.join('/etc/systemd/system', filename)
     here = path.abspath(path.dirname(__file__))
-    with open(path.join(here, filename), encoding='utf-8') as f:
+    with open(path.join(here, filename), 'r') as f:
         service = f.read()
     service = service.format(working_dir=here, exec_start='python ' + here)
     with open(install_path, 'w') as f:
