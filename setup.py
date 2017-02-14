3637a9d1439beaf12185c5acebd40992032c5a46
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-14 18:46:53 +0200

    syntax fix

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
             'console_scripts': [
-                    'install_ilmaruuvi_service = {name}.systemd_service:install'.format(name)
+                    'install_ilmaruuvi_service = {name}.systemd_service:install'.format(name=name)
             ]
     },
 )
