e86e1d53d975ae58fa7287b7cfdab05fdf95e6cc
Jussi Tiira <jussi@j24.fi>
2017-02-18 20:58:05 +0200

    day by day datafile

--- ilmaruuvi/__main__.py
+++ ilmaruuvi/__main__.py
@@ -4,18 +4,19 @@ from datetime import datetime
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
 def main():
-    database_file = '/var/lib/ilmaruuvi/db.hdf'
     logfile = '/var/log/ilmaruuvi.log'
     MAC1 = 'E8:A8:F2:B1:AE:A3'
     sensor = RuuviTagSensor(MAC1)
     while True:
         state = sensor.update()
+        now = datetime.today()
+        database_file = '/var/lib/ilmaruuvi/{}.csv'.format(now.strftime(%Y%m%d))
         with open(logfile, 'a') as f:
-            f.write('{time},{t},{rh},{p}\n'.format(time=datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+            f.write('{time},{t},{rh},{p}\n'.format(time=now.strftime('%Y-%m-%d %H:%M:%S'),
 						 t=state['temperature'],
 					   	 rh=state['humidity'],
 					    	 p=state['pressure']))
-        time.sleep(10*60)
+        time.sleep(880)
 
 if __name__ == "__main__":
     # execute only if run as a script
